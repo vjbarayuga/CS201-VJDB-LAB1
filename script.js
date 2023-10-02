65ddfe0d8123da461c46409d7750ea5057bb7a38
vjdbarayuga <vjdbarayuga@gmail.com>
2023-10-02 22:33:27 +0800

    feat: added javascript code into my html

---
+++ script.js
@@ -0,0 +1,55 @@
+// AN EXAMPLE OF A COMMENT
+
+// NUMBERS
+
+// const randomNumber = 1000;
+// let number = 20;
+
+// // BASIC MATH
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(3 / 1);
+// console.log(number++);
+// console.log(number--);
+
+//BOOLEAN
+//true, false, undefined, 0, -0, NaN
+// let isLoggedIn = NaN;
+// let numbers = 10;
+// console.log(numbers + isLoggedIn);
+
+//COMPARISON
+//greater than, less than, greater or equal to, less than or equal to
+// console.log(10 > 10);
+// console.log(2 < 10);
+// console.log(10 >= 5);
+// console.log(10 <= 10);
+
+//STRING or STRINGS
+//concatenation, append, length, cases, slice, split and join, includes, trim
+let firstName = 'VJ';
+let lastName = 'BARAYUGA';
+
+let fullName = firstName + '' + lastName;
+// let fullName = firstName.concat(lastName);
+// console.log(fullName);
+
+let upperName = 'thequickbrownfox';
+let trimMethod = '         this is the trim method';
+
+// firstName += 'something else was appended';
+// console.log(firstName);
+
+//console.log(lastName.length);
+
+// console.log(lastName.toLowerCase());
+// console.log(upperName.toUpperCase());
+
+//console.log(fullName.slice(0, 8));
+
+//console.log(fullName.split('').join('-'));
+
+//console.log(fullName.includes('X'));
+
+//console.log(trimMethod.trim());
